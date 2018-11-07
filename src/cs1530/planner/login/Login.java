424e067621329b65337dc3166979757cee57424b
mcwilleg <mcwilleg@gmail.com>
2018-11-06 21:27:22 -0500

    Updated login

--- src/cs1530/planner/login/Login.java
+++ src/cs1530/planner/login/Login.java
@@ -7,46 +7,27 @@ package cs1530.planner.login;
 
 import cs1530.planner.calendar.UserProfile;
 import cs1530.planner.database.Database;
+
 import java.util.Set;
-import java.util.HashSet;
 
 
-public final class Login{
-	
+public final class Login {
 	/*
 	*static method to take a username password and list of profiles to validate 
-	*returns null currentllly if not found we can change this depending on how we want to handle it
+	*returns null currently if not found we can change this depending on how we want to handle it
 	*/
-	public static UserProfile userLogin(String username, String password, Set<UserProfile> profiles){
-		
+	public static UserProfile userLogin(String username, String password, Database database) {
 		//user is return value exists is flag to find profile
-		boolean exists = false;
-		UserProfile user = null;
-			
-		//search for username in list of profiles
-		for(UserProfile profile: profiles){
-			if(username.equals(profile.getUsername())){
-				exists = true;
-				user = profile;
-				break;
-			}
-		}
-		
-		//if profile not in list rurn null could display message in ui suggesting to create profile
-		if(!exists){
+		UserProfile user;
+		if((user = database.getProfile(username)) == null)
 			return null;
-		}
 		
-		//validate password if correct return user 
-		if(user.verifyPassword(password)){
-			return user;
-		}else{
-			return null;
-		}
+		//validate password if correct return user
+		return user.verifyPassword(password) ? user : null;
 	}
 	
 	//adds new user to the set then returns said profile so that user is logged in and use their new calendar
-	public static UserProfile createProfile(String username, String password, Set<UserProfile> profiles){
+	public static UserProfile createProfile(String username, String password, Set<UserProfile> profiles) {
 		UserProfile newUser = new UserProfile(username, password);
 		profiles.add(newUser);
 		return newUser;
