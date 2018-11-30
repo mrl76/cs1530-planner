37127fc8f6dc754d8198e3c00899d082f90ad6f5
mcwilleg <mcwilleg@gmail.com>
2018-11-29 22:36:04 -0500

    Connected ProfileController to UI

--- src/cs1530/planner/ui/profile/ProfileController.java
+++ src/cs1530/planner/ui/profile/ProfileController.java
@@ -1,17 +1,38 @@
 package cs1530.planner.ui.profile;
 
+import cs1530.planner.Main;
+import cs1530.planner.calendar.UserProfile;
+import cs1530.planner.calendar.event.Appointment;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class ProfileController {
-	@FXML
-	public Label username;
+	@FXML public Label username;
+	@FXML public Button addButton, filterButton;
+	@FXML public VBox eventBox;
 	
-	@FXML
-	public Button addButton, filterButton;
+	private UserProfile profile;
 	
-	@FXML
-	public VBox eventBox;
+	@FXML private void initialize() {
+		this.profile = Main.getOpenProfile();
+		username.setText(profile.getUsername());
+	}
+	
+	public void onAddButton() {
+		//TODO open new event window
+		Appointment event = new Appointment("placeholder"); //new event window returns new event
+		profile.getCalendar().addAppointment(event);
+		refreshDisplay();
+	}
+	
+	public void onFilterButton() {
+		//TODO open filter window
+		refreshDisplay();
+	}
+	
+	private void refreshDisplay() {
+		//TODO create display
+	}
 }
