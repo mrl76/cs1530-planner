606d034600346368659b57e8ab07c60bc414019f
mcwilleg <mcwilleg@gmail.com>
2018-12-01 00:24:28 -0500

    Updated login window to close automatically

--- src/cs1530/planner/ui/login/LoginController.java
+++ src/cs1530/planner/ui/login/LoginController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
 	@FXML public TextField username;
@@ -40,6 +41,7 @@ public class LoginController {
 		else {
 			error.setText("");
 			main.openProfile(user);
+			((Stage) username.getScene().getWindow()).close();
 		}
 	}
 }
